fb16c66045c556e9ca39938e86013adda881c073
Remco Abraham <remco.abraham@navara.nl>
2022-01-02 11:23:30 +0100

    day 4 - part 1

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -5,7 +5,7 @@ pub struct Day4;
 
 pub struct Input {
     draws: Vec<u32>,
-    boards: Vec<[[u32; 5]; 5]>
+    boards: Vec<[[u32; 5]; 5]>,
 }
 
 impl Day<Input> for Day4 {
@@ -47,15 +47,62 @@ impl Day<Input> for Day4 {
         }
         Input {
             draws,
-            boards
+            boards,
         }
     }
 
     fn part1(input: &Input) -> u32 {
-        todo!()
+        let (drawn_numbers, last_draw, board_index) = Self::get_winning_board(input);
+        Self::get_score(&input.boards[board_index], &drawn_numbers, last_draw)
     }
 
     fn part2(input: &Input) -> u32 {
         todo!()
+    }
+}
+
+impl Day4 {
+    fn get_winning_board(input: &Input) -> ([[u32; 5]; 5], u32, usize) {
+        let mut boards: Vec<[[u32; 5]; 5]> = vec![Default::default(); input.boards.len()];
+        for &draw in &input.draws {
+            for board in 0..boards.len() {
+                for row in 0..5 {
+                    for col in 0..5 {
+                        if input.boards[board][row][col] == draw {
+                            boards[board][row][col] = 1;
+                            let mut has_row = true;
+                            let mut has_col = true;
+                            for check in 0..5 {
+                                if boards[board][check][col] == 0 {
+                                    has_col = false;
+                                }
+                                if boards[board][row][check] == 0 {
+                                    has_row = false;
+                                }
+                                if !has_row && !has_col {
+                                    break;
+                                }
+                            }
+                            if has_row || has_col {
+                                return (boards[board], draw, board);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        panic!("Did not find a winning board!");
+    }
+
+    fn get_score(board: &[[u32; 5]; 5], drawn_numbers: &[[u32; 5]; 5], draw: u32) -> u32 {
+        let mut sum: u32 = 0;
+        for row in 0..5 {
+            for col in 0..5 {
+                if drawn_numbers[row][col] == 0 {
+                    sum += board[row][col];
+                }
+            }
+        }
+        sum * draw
     }
 }
