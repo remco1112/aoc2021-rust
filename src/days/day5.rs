4139861593c16ee0d2814cea8511704e5766571a
Remco Abraham <remco.abraham@navara.nl>
2022-01-02 13:40:12 +0100

    day 5 - part 1

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,20 +1,21 @@
+use std::cmp::{max, min};
 use std::io::{BufRead};
 use regex::Regex;
 use crate::days::day::Day;
 
 pub struct Day5;
 
-impl Day<Vec<((u32, u32), (u32, u32))>> for Day5 {
-    fn parse(reader: &mut dyn BufRead) -> Vec<((u32, u32), (u32, u32))> {
+impl Day<Vec<((usize, usize), (usize, usize))>> for Day5 {
+    fn parse(reader: &mut dyn BufRead) -> Vec<((usize, usize), (usize, usize))> {
         let regex = Regex::new(r"^(\d+),(\d+) -> (\d+),(\d+)\n?$").unwrap();
         let mut line = String::with_capacity(11);
-        let mut lines: Vec<((u32, u32), (u32, u32))> = Default::default();
+        let mut lines: Vec<((usize, usize), (usize, usize))> = Default::default();
         while reader.read_line(&mut line).unwrap() > 0 {
             if let [x_1, y_1, x_2, y_2] = regex.captures(&line).unwrap()
                 .iter()
                 .skip(1)
-                .map(|n| n.unwrap().as_str().parse::<u32>().unwrap())
-                .collect::<Vec<u32>>()[..] {
+                .map(|n| n.unwrap().as_str().parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()[..] {
                 lines.push(((x_1, y_1), (x_2, y_2)));
                 line.clear();
                 continue;
@@ -24,11 +25,43 @@ impl Day<Vec<((u32, u32), (u32, u32))>> for Day5 {
         lines
     }
 
-    fn part1(input: &Vec<((u32, u32), (u32, u32))>) -> u32 {
-        todo!()
+    fn part1(input: &Vec<((usize, usize), (usize, usize))>) -> u32 {
+        Self::get_overlap_count(&Self::get_grid(input))
     }
 
-    fn part2(input: &Vec<((u32, u32), (u32, u32))>) -> u32 {
+    fn part2(input: &Vec<((usize, usize), (usize, usize))>) -> u32 {
         todo!()
+    }
+}
+
+impl Day5 {
+    fn get_grid(input: &Vec<((usize, usize), (usize, usize))>) -> [[usize; 1000]; 1000] {
+        let mut grid = [[0; 1000]; 1000];
+        for &line in input {
+            let ((x_1, y_1), (x_2, y_2)) = line;
+            if x_1 == x_2 {
+                for y in min(y_1, y_2)..=max(y_1, y_2) {
+                    grid[y][x_1] += 1;
+                }
+            }
+            if y_1 == y_2 {
+                for x in min(x_1, x_2)..=max(x_1, x_2) {
+                    grid[y_1][x] += 1;
+                }
+            }
+        }
+        grid
+    }
+
+    fn get_overlap_count(grid: &[[usize; 1000]; 1000]) -> u32 {
+        let mut n: u32 = 0;
+        for row in grid {
+            for &col in row {
+                if col > 1 {
+                    n += 1
+                }
+            }
+        }
+        n
     }
 }
